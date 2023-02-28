5a34e7f4b1955605d949db8745a9fe48c22feb16
vhlahti <henri.veholahti@gmail.com>
2023-02-28 11:29:26 +0200

    add phonelist component and phone service

---
+++ src/app/phonelist/phonelist.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PhonelistComponent } from './phonelist.component';
+
+describe('PhonelistComponent', () => {
+  let component: PhonelistComponent;
+  let fixture: ComponentFixture<PhonelistComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PhonelistComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(PhonelistComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/phonelist/phonelist.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-phonelist',
+  templateUrl: './phonelist.component.html',
+  styleUrls: ['./phonelist.component.css']
+})
+export class PhonelistComponent {
+
+}
