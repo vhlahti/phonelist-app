aaa285dda6de7982808a8414c18314f9eb83f019
vhlahti <henri.veholahti@gmail.com>
2023-02-28 16:45:24 +0200

    update phonelist to use service, add a template to test its functioning

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PhonelistComponent } from './phonelist/phonelist.component';
+import { PhoneDetailComponent } from './phonelist/phone-detail/phone-detail.component';
+import { PhoneService } from './phone.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PhonelistComponent
+    PhonelistComponent,
+    PhoneDetailComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [PhoneService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/phonelist/phonelist.component.ts
+++ src/app/phonelist/phonelist.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { PhoneService } from '../phone.service';
 
 @Component({
   selector: 'app-phonelist',
   templateUrl: './phonelist.component.html',
   styleUrls: ['./phonelist.component.css']
 })
-export class PhonelistComponent {
+export class PhonelistComponent implements OnInit {
+  products: Observable<any>;
+  constructor(private phoneService: PhoneService) {}
 
+  ngOnInit(): void {
+    this.products = this.phoneService.getAllProducts();
+    // this.getProducts();
+  }
+
+  // alternate option if not using Observable:
+
+  // getProducts(): void {
+  //   this.phoneService.getAllProducts().subscribe((data) => {this.productList = data;
+  //   },
+  //   (error) => {
+  //     console.log('http-error:');
+  //     console.log(error);
+  //   });
 }
