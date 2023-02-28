a954fcd62cf82699b0a13c7317f978d8b41139bc
vhlahti <henri.veholahti@gmail.com>
2023-02-28 11:47:00 +0200

    configure service to fetch data from json database

--- src/app/phone.service.ts
+++ src/app/phone.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class PhoneService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAllProducts(): any {
+    const products = this.http.get('assets/phones/phones.json');
+    return products;
+  }
+
+  getProduct(id: string): any {
+    const product = this.http.get('assets/phones/' + id + '.json');
+    return product;
+  }
+  
 }
