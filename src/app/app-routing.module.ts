7b73afd8c001a3e7db2f9f3d3d0e1fbe3e067516
vhlahti <henri.veholahti@gmail.com>
2023-03-01 18:27:47 +0200

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PhoneDetailComponent } from './phonelist/phone-detail/phone-detail.component';
+import { PhonelistComponent } from './phonelist/phonelist.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: PhonelistComponent},
+  {path: 'home', component: PhonelistComponent},
+  {path: 'details/:id', component: PhoneDetailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
